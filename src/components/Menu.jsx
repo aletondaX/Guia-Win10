2488ccaefa15842645add0ec95272bccdf30ced3
aletondaX <gtrrsta@outlook.com>
2023-12-17 20:41:21 -0300

    Installed react router

---
+++ src/components/Menu.jsx
@@ -0,0 +1,13 @@
+export default function Menu() {
+  return (
+    <nav>
+      <ol>
+        <li>Introducción</li>
+        <li>Pre-Instalación</li>
+        <li>Post-Instalación</li>
+        <li>Extras</li>
+        <li>Solución de Problemas</li>
+      </ol>
+    </nav>
+  );
+}
