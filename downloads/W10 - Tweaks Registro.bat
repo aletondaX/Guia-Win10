6313ba65f676684416523217d851348f617e12a9
aletondaX <gtrrsta@outlook.com>
2024-03-27 19:51:07 -0300

    Updates

--- downloads/W10 - Tweaks Registro.bat
+++ downloads/W10 - Tweaks Registro.bat
@@ -2,6 +2,13 @@
 chcp 65001 >nul
 title Tweaks de registro para Windows 10 by AleTonda
 
+echo [45m                                                   [0m
+echo [45m  Tweaks de registro para Windows 10, by AleTonda  [0m
+echo [45m                                                   [0m
+
+timeout /t 3 /nobreak >nul
+
+:seleccionPC
 echo.
 echo Selecciona tu tipo de PC:
 echo.
@@ -10,30 +17,32 @@ echo 2. Notebook
 echo.
 SET /P PC=Escribe [45m 1 [0m o [45m 2 [0m y presiona ENTER para continuar:
 
+IF %PC% NEQ 1 call :comprobacionDos
+
 echo.
 echo Aplicando cambios al registro...
-timeout /t 3 /nobreak >nul
+timeout /t 2 /nobreak >nul
 echo.
 
-echo Agregar ubicaciones favoritas al registro
+echo [1mAgregar ubicaciones favoritas al registro[0m
 timeout /t 1 /nobreak >nul
 reg add "HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Applets\Regedit\Favorites" /v "Image File Execution Options" /t REG_SZ /d "Equipo\HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Image File Execution Options" /f
 reg add "HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Applets\Regedit\Favorites" /v "Services" /t REG_SZ /d "Equipo\HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Services" /f
 reg add "HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Applets\Regedit\Favorites" /v "Tasks" /t REG_SZ /d "Equipo\HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Schedule\TaskCache" /f
 echo.
-echo Configurar PowerShell en Unrestricted
+echo [1mConfigurar PowerShell en Unrestricted[0m
 timeout /t 1 /nobreak >nul
 reg add "HKLM\SOFTWARE\Microsoft\PowerShell\1\ShellIds\Microsoft.PowerShell" /v "Path" /t REG_SZ /d "C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe" /f
 reg add "HKLM\SOFTWARE\Microsoft\PowerShell\1\ShellIds\Microsoft.PowerShell" /v "ExecutionPolicy" /t REG_SZ /d "Unrestricted" /f
 reg add "HKLM\SOFTWARE\Wow6432Node\Microsoft\PowerShell\1\ShellIds\Microsoft.PowerShell" /v "Path" /t REG_SZ /d "C:\Windows\SysWOW64\WindowsPowerShell\v1.0\powershell.exe" /f
 reg add "HKLM\SOFTWARE\Wow6432Node\Microsoft\PowerShell\1\ShellIds\Microsoft.PowerShell" /v "ExecutionPolicy" /t REG_SZ /d "Unrestricted" /f
 echo.
-echo Deshabilitar apps en segundo plano
+echo [1mDeshabilitar apps en segundo plano[0m
 timeout /t 1 /nobreak >nul
 reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\BackgroundAccessApplications" /v "GlobalUserDisabled" /t REG_DWORD /d "1" /f
 reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Search" /v "BackgroundAppGlobalToggle" /t REG_DWORD /d "0" /f
 echo.
-echo Deshabilitar GameDVR
+echo [1mDeshabilitar GameDVR[0m
 timeout /t 1 /nobreak >nul
 reg add "HKCU\System\GameConfigStore" /v "GameDVR_Enabled" /t REG_DWORD /d "0" /f
 reg add "HKCU\System\GameConfigStore" /v "GameDVR_FSEBehaviorMode" /t REG_DWORD /d "2" /f
@@ -45,21 +54,21 @@ reg add "HKLM\SOFTWARE\Microsoft\PolicyManager\default\ApplicationManagement\All
 reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\GameDVR" /v "AppCaptureEnabled" /t REG_DWORD /d "0" /f
 reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows\GameDVR" /v "AllowGameDVR" /t REG_DWORD /d "0" /f
 echo.
-echo Deshabilitar GPU Preemption
+echo [1mDeshabilitar GPU Preemption[0m
 timeout /t 1 /nobreak >nul
 reg add "HKLM\SYSTEM\CurrentControlSet\Control\GraphicsDrivers\Scheduler" /v "EnablePreemption" /t REG_DWORD /d "0" /f
 echo.
-echo Deshabilitar hibernación
+echo [1mDeshabilitar hibernación[0m
 timeout /t 1 /nobreak >nul
 reg add "HKLM\SYSTEM\CurrentControlSet\Control\Power" /v "HibernateEnabledDefault" /t REG_DWORD /d "0" /f
 echo.
-echo Deshabilitar Spectre y Meltdown
+echo [1mDeshabilitar Spectre y Meltdown[0m
 timeout /t 1 /nobreak >nul
 reg add "HKLM\SYSTEM\CurrentControlSet\Control\Session Manager\Memory Management" /v "FeatureSettings" /t REG_DWORD /d "1" /f
 reg add "HKLM\SYSTEM\CurrentControlSet\Control\Session Manager\Memory Management" /v "FeatureSettingsOverride" /t REG_DWORD /d "3" /f
 reg add "HKLM\SYSTEM\CurrentControlSet\Control\Session Manager\Memory Management" /v "FeatureSettingsOverrideMask" /t REG_DWORD /d "3" /f
 echo.
-echo Tweaks varios para juegos y responsividad del sistema 
+echo [1mTweaks varios para juegos y responsividad del sistema[0m
 timeout /t 1 /nobreak >nul
 reg add "HKLM\SYSTEM\CurrentControlSet\Control\PriorityControl" /v "Win32PrioritySeparation" /t REG_DWORD /d "38" /f
 reg add "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Multimedia\SystemProfile" /v "NetworkThrottlingIndex" /t REG_DWORD /d "4294967295" /f
@@ -72,17 +81,18 @@ reg add "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Multimedia\SystemProf
 reg add "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Multimedia\SystemProfile\Tasks\Games" /v "Scheduling Category" /t REG_SZ /d "High" /f
 reg add "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Multimedia\SystemProfile\Tasks\Games" /v "SFIO Priority" /t REG_SZ /d "High" /f
 echo.
-echo Desactivar notificaciones de seguridad
+echo [1mDesactivar notificaciones de seguridad[0m
 timeout /t 1 /nobreak >nul
 reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows Defender Security Center\Notifications" /v "DisableEnhancedNotifications" /t REG_DWORD /d "1" /f
 reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows Defender Security Center\Notifications" /v "DisableNotifications" /t REG_DWORD /d "1" /f
-If /I %PC%==1 (
+IF %PC%==1 (
 echo.
-echo Deshabilitar Power Throttling
+echo [1mDeshabilitar Power Throttling[0m
 timeout /t 1 /nobreak >nul
 reg add "HKLM\SYSTEM\CurrentControlSet\Control\Power\PowerThrottling" /v "PowerThrottlingOff" /t REG_DWORD /d "1" /f
 echo.
-echo Habilitar Task Offloading
+echo [1mHabilitar Task Offloading[0m
+timeout /t 1 /nobreak >nul
 reg add "HKLM\SYSTEM\CurrentControlSet\Services\Tcpip\Parameters" /v "DisableTaskOffload" /t REG_DWORD /d "0" /f
 )
 echo.
@@ -94,3 +104,9 @@ echo #######################################################
 echo.
 SET /P REINICIAR=Reiniciar el equipo? (S/N):
 If /I %REINICIAR%==S (shutdown -r -t 3) else exit
+
+:comprobacionDos
+IF %PC% NEQ 2 (
+cls
+goto :seleccionPC
+) ELSE goto :eof
